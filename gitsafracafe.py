233e387bbdd901a3ac27c7a672f32677440f9f91
safradocafe <mariovisk@gmail.com>
2025-08-01 18:33:57 -0300

    Update gitsafracafe.py

--- gitsafracafe.py
+++ gitsafracafe.py
@@ -3,7 +3,6 @@
 # Este projeto tem o objetivo de desensolver uma geotecnologia para previsão da produtividade do café com o uso de imagens do sensor MSI/Sentinel-2A e algoritmos de machine learning.
 # Este trabalho É resultado de pesquisa acadêmica de Mestrado Profissional em Agricultura de Precisão do Colégio Politécnico da Universidade Federal de Santa Maria (UFSM).
 
-print("Fiona não está instalada. Instale manualmente via requirements.txt.")
 # Imports essenciais
 import os
 import json
@@ -11,46 +10,41 @@ import time
 import random
 import string
 import numpy as np
+import pandas as pd
+import zipfile
+
 import geemap
 import geocoder
 import ee
 import geopandas as gpd
-from shapely.geometry import Point, mapping, shape as shapely_shape
-from IPython.display import display
-from ipywidgets import FileUpload
+import speech_recognition as sr
+
+from shapely.geometry import Point, mapping, shape as shapely_shape, shape
+from ipyfilechooser import FileChooser
 from ipywidgets import (
-    Button, Dropdown, Output, VBox, HBox, HTML, FloatText,
+    FileUpload, Button, Dropdown, Output, VBox, HBox, HTML, FloatText,
     ToggleButton, Layout, Checkbox
 )
-import speech_recognition as sr
 from IPython.display import display, clear_output
-import zipfile
-import pandas as pd
 from geemap import geojson_to_ee
-import speech_recognition as sr
-from ipyfilechooser import FileChooser
-from shapely.geometry import shape
+
+# Verificação da biblioteca Fiona
 try:
     import fiona
     from fiona import drivers
     HAS_FIONA = True
 except ImportError:
     HAS_FIONA = False
-    print("Instalando biblioteca Fiona...")
-    !pip install fiona
-    try:
-        import fiona
-        from fiona import drivers
-        HAS_FIONA = True
-    except ImportError:
-        print("Não foi possível instalar a biblioteca Fiona. Algumas funcionalidades serão limitadas.")
+    print("Fiona não está instalada. Instale manualmente via requirements.txt.")
 
 # Autenticação e inicialização do Earth Engine
 try:
     ee.Initialize(project='agriculturadeprecisao')
-except:
+except Exception as e:
+    print("Falha ao inicializar o Earth Engine. Tentando autenticar...")
     ee.Authenticate()
     ee.Initialize(project='agriculturadeprecisao')
+
 
 # Inicializa o mapa
 Map = geemap.Map(center=[-15, -55], zoom=4)
