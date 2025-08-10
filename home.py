ae814bf08fc7196a4d459065018cf75cb3cd19ef
safradocafe <mariovisk@gmail.com>
2025-08-10 09:18:02 -0300

    Update home.py

--- home.py
+++ home.py
@@ -10,7 +10,8 @@ st.title("☕ Safra do Café")
 st.markdown("""
 Sistema avançado de previsão da produtividade do café com imagens de satélite (MSI/Sentinel-2A) e algoritmos de Machine Learning.
 """)
-st.title("##*Acesse o sistema pelas barras laterais*##)
+st.markdown("## *Acesse o sistema pelas barras laterais*")  # Corrigido: st.markdown em vez de st.title
+
 # Carregando as imagens
 img1 = Image.open(r"D:\Arquivos_R\sr\sr_cafe\Santa_Vera\teste app\indices_capa.jpeg")
 img2 = Image.open(r"D:\Arquivos_R\sr\sr_cafe\Santa_Vera\teste app\img_capa_prod.jpeg")
@@ -23,5 +24,4 @@ with col1:
 
 with col2:
     st.image(img2, caption="... gerar o seu mapa de produtividade", use_column_width=True)
-
 
