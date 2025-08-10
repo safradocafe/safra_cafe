30658e2589a7842ce2a5e7daf704d3e9b38c2296
safradocafe <mariovisk@gmail.com>
2025-08-10 09:15:59 -0300

    Update Início.py

--- Início.py
+++ Início.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(
     page_title="Safra do Café",
@@ -7,8 +8,20 @@ st.set_page_config(
 
 st.title("☕ Safra do Café")
 st.markdown("""
-Bem-vindo ao sistema de análise da **Safra do Café**.
-Use o menu lateral para navegar entre as páginas.
+Sistema avançado de previsão da produtividade do café com imagens de satélite (MSI/Sentinel-2A) e algoritmos de Machine Learning.
 """)
+st.title("##*Acesse o sistema pelas barras laterais*##)
+# Carregando as imagens
+img1 = Image.open(r"D:\Arquivos_R\sr\sr_cafe\Santa_Vera\teste app\indices_capa.jpeg")
+img2 = Image.open(r"D:\Arquivos_R\sr\sr_cafe\Santa_Vera\teste app\img_capa_prod.jpeg")
+
+# Criando duas colunas para as imagens
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image(img1, caption="Combinamos índices espectrais e Machine Learning para...", use_column_width=True)
+
+with col2:
+    st.image(img2, caption="... gerar o seu mapa de produtividade", use_column_width=True)
 
 
