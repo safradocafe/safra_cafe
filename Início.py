5ea48cb41e5f05223b4f4b2cef7a56d223697e19
safradocafe <mariovisk@gmail.com>
2025-08-10 09:10:07 -0300

    Update Início.py

--- Início.py
+++ Início.py
@@ -12,21 +12,4 @@ Bem-vindo ao sistema de análise da **Safra do Café**.
 Use o menu lateral para navegar entre as páginas.
 """)
 
-# Carregando as imagens
-img1 = Image.open(r"D:\Arquivos_R\sr\sr_cafe\Santa_Vera\teste app\indices_capa.jpeg")
-img2 = Image.open(r"D:\Arquivos_R\sr\sr_cafe\Santa_Vera\teste app\img_capa_prod.jpeg")
 
-# Criando duas colunas para as imagens
-col1, col2 = st.columns(2)
-
-with col1:
-    st.image(img1, caption="Combinamos índices espectrais e Machine Learning para...", use_column_width=True)
-
-with col2:
-    st.image(img2, caption="... gerar o seu mapa de produtividade", use_column_width=True)
-
-st.markdown("""
-<p style='text-align: center; font-size: 16px; margin-top: 10px;'>
-    Combinamos índices espectrais e Machine Learning para gerar o seu mapa de produtividade
-</p>
-""", unsafe_allow_html=True)
