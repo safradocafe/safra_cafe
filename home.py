362ef617e960ae409d19ebd946447b154866b426
Safra do Café <mariovisk@gmail.com>
2025-08-12 15:58:44 -0300

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
 import os
+
 port = int(os.environ.get("PORT", 8501))
 st.set_page_config(page_title="Safra do café", layout="wide")
 st.set_page_config(
