daf65c605831c7fa6ea43b64d40d0a79ae94a1f6
Safra do Café <mariovisk@gmail.com>
2025-10-14 22:25:21 -0300

    Create 4.0_Previsão_da_produtividade.py

---
+++ pages/4.0_Previsão_da_produtividade.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.set_page_config(page_title="Previsão da produtividade", layout="wide")
+st.markdown("# Previsão da produtividade")
+st.page_link("pages/4.1_Análise_de_correlação.py", label="Análise de correlação")
+st.page_link("pages/4.2_Treinamento_com_Machine_Learning.py", label="Treinamento com Machine Learning")
+st.page_link("pages/4.3_Previsão_da_safra.py", label="Previsão da safra")
