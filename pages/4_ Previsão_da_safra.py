b93365a34b2d2354b7243b8908f6936a4ee94d63
Safra do Café <mariovisk@gmail.com>
2025-10-16 19:47:01 -0300

    Update 4_ Previsão_da_safra.py

--- pages/4_ Previsão_da_safra.py
+++ pages/4_ Previsão_da_safra.py
@@ -1,6 +1,8 @@
 import streamlit as st
 st.set_page_config(page_title="Previsão da safra", layout="wide")
 st.markdown("# Previsão da safra")
-st.page_link("pages/4_1. Análise_de_correlação.py", label="Análise de correlação")
-st.page_link("pages/4_2. Treinamento_com_Machine_Learning.py", label="Treinamento com Machine Learning")
-st.page_link("pages/4_3. Previsão_da_produtividade.py", label="Previsão da produtividade")
+
+st.page_link("pages/4_1_Analise_de_correlacao.py", label="1. Análise de correlação")
+st.page_link("pages/4_2_Treinamento_com_Machine_Learning.py", label="2. Treinamento com Machine Learning")
+st.page_link("pages/4_3_Prev_produtividade.py", label="3. Previsão da produtividade")
+
